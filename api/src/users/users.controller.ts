c6de4fc8e03e70368d855007ba3cc8236fde6127
Lucas Satelos <lcsdegodoy@outlook.com>
2025-03-05 12:26:50 -0300

    remove unnecessary assertion

--- api/src/users/users.controller.ts
+++ api/src/users/users.controller.ts
@@ -57,7 +57,7 @@ export class UsersController {
         ) => {
           const uniqueSuffix =
             Date.now() + '-' + Math.round(Math.random() * 1e9);
-          const ext = extname(file.originalname as string);
+          const ext = extname(file.originalname);
           cb(null, `${file.fieldname}-${uniqueSuffix}${ext}`);
         },
       }),

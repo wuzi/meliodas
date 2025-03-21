8cf9a9dc416f00ad572b9d9cfad435396f61a8b4
Lucas Godoy <lcsdegodoy@outlook.com>
2025-03-20 22:03:13 -0300

    add faker dependency and implement populate route to generate test data for users and materials

--- api/src/app.controller.ts
+++ api/src/app.controller.ts
@@ -1,7 +1,17 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 import { UsersService } from './users/users.service';
 import { MaterialsService } from './materials/materials.service';
+import { faker } from '@faker-js/faker';
+import {
+  UserCategory,
+  UserProfile,
+  UserStatus,
+} from './users/entities/user.entity';
+import { MaterialType, Status } from './materials/entities/material.entity';
 
 @Controller()
 export class AppController {
@@ -21,5 +31,48 @@ export class AppController {
     const users = await this.usersService.count();
     const materials = await this.materialsService.count();
     return { users, materials };
+  }
+
+  @Get('populate') // Get, so we can access this route from the browser
+  async populate() {
+    const users = [];
+    const materials = [];
+
+    // Create 10 random users
+    for (let i = 0; i < 10; i++) {
+      const user = await this.usersService.create({
+        name: faker.person.fullName(),
+        email: faker.internet.email(),
+        password: faker.internet.password(),
+        phone: faker.phone.number(),
+        profile: faker.helpers.arrayElement(Object.values(UserProfile)),
+        category: faker.helpers.arrayElement(Object.values(UserCategory)),
+        registrationNumber: faker.string.numeric(8),
+        course: faker.person.jobTitle(),
+        department: faker.company.catchPhraseNoun(),
+        status: UserStatus.Active,
+      });
+      users.push(user);
+    }
+
+    // Create 10 random materials
+    for (let i = 0; i < 10; i++) {
+      const material = await this.materialsService.create({
+        name: faker.commerce.productName(),
+        description: faker.commerce.productDescription(),
+        type: faker.helpers.arrayElement(Object.values(MaterialType)),
+        patrimonyNumber: faker.string.numeric(6),
+        status: Status.Active,
+      });
+      materials.push(material);
+    }
+
+    return {
+      message: 'Database populated with test data',
+      created: {
+        users: users.length,
+        materials: materials.length,
+      },
+    };
   }
 }
